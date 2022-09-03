4d7a51adffbacd2e43dc09ea6d93aee8592ced02
zly <1876400170@qq.com>
2022-09-03 20:34:24 +0800

    master commit1

--- src/main/java/com/zly/git/GitTest.java
+++ src/main/java/com/zly/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("master git!");
     }
 }
