4d248aa4303208611bcc786037386662e5d932aa
zly <1876400170@qq.com>
2022-09-03 19:59:38 +0800

    first commit

---
+++ src/main/java/com/zly/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zly.git;
+
+/**
+ * @author zly
+ * @create 2022-09-03 19:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
